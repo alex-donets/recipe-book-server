83a47f068f3381974e6fcac935efc04650982d21
Veronika Donets <veronikad@play-consult.co.uk>
2020-12-27 18:43:30 +0200

    fix schedule

--- schedulers/schedule-list.js
+++ schedulers/schedule-list.js
@@ -2,8 +2,8 @@ const schedule = require('node-schedule');
 const Chat = require('../models/chat');
 
 schedule.scheduleJob({
-    hour: 22,
-    minute: 37,
+    hour: 0,
+    minute: 0,
     dayOfWeek: [0, 1, 2, 3, 4, 5, 6]
 }, function() {
    Chat.deleteAllMessages((err) => {
